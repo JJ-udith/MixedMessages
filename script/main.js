4590e4c69569d3bb5f3130325dd9e14988e98257
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-11-09 14:19:22 +0330

    Added Fade Effect based on JQuery

--- script/main.js
+++ script/main.js
@@ -1,15 +1,25 @@
+const headline = document.getElementById("headline");
+const card = document.getElementById("card");
+const footer = document.getElementById("footer");
 const form = document.getElementById("form");
-const submitButton = document.getElementById("submit-btn");
 const nameElement = document.getElementById("name");
+const submitButton = document.getElementById("submit-btn");
+const partOne = document.getElementById("part-one");
 
-// Fade Effect
-const fadeEffect = (element) => {
-  element.className = "fadeIn";
-  setTimeout(() => {
-    $(element).css("display", "none");
-  }, 1000);
+// Fade Effect on Page Load
+$(document).ready(() => $(card).fadeTo(2000, 0.9));
+$(document).ready(() => $(footer).fadeTo(2000, 0.5));
+$(document).ready(() => $(headline).fadeIn(2000));
+
+// Fade Effect for Elements
+const fadeEffect = (element1, element2) => {
+  $(element1).fadeOut(1000, () => {
+    $(element2).fadeIn(2000);
+  });
 };
 
-const clickAction = () => {
-  fadeEffect(form);
+const submitAction = () => {
+  if (nameElement.value) {
+    fadeEffect(form, partOne);
+  }
 };
